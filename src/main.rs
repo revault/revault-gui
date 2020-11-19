e231415f7051fba82496d38d920669071dd0c61b
edouard <edouard@revault.dev>
2020-11-19 22:26:33 +0100

    feat(revaultd): add client and process start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env::VarError;
+use std::path::PathBuf;
+mod revaultd;
+
+extern crate serde;
+extern crate serde_json;
+
 fn main() {
-    println!("Hello, world!");
+    let path = match std::env::var("REVAULTD_CONF") {
+        Ok(p) => Some(PathBuf::from(p)),
+        Err(VarError::NotPresent) => None,
+        Err(VarError::NotUnicode(_)) => {
+            println!("Error: REVAULTD_CONF path has a wrong unicode format");
+            std::process::exit(1);
+        }
+    };
+
+    if let Some(p) = path {
+        revaultd::RevaultD::new(p).unwrap();
+    }
 }
