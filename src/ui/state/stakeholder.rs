7db51d9849cf46fc7333ad8e7ba0d923af4bcc3a
edouard <edouard@revault.dev>
2021-01-08 15:53:20 +0100

    state: stakeholder

---
+++ src/ui/state/stakeholder.rs
@@ -0,0 +1,38 @@
+use std::sync::Arc;
+
+use iced::{Command, Element};
+
+use super::State;
+use crate::revaultd::RevaultD;
+use crate::ui::message::Message;
+
+#[derive(Debug)]
+pub struct StakeholderState {
+    revaultd: Arc<RevaultD>,
+}
+
+impl StakeholderState {
+    pub fn new(revaultd: Arc<RevaultD>) -> Self {
+        StakeholderState { revaultd }
+    }
+}
+
+impl State for StakeholderState {
+    fn update(&mut self, message: Message) -> Command<Message> {
+        Command::none()
+    }
+
+    fn view<'a>(&'a mut self) -> Element<Message> {
+        iced::Container::new(iced::Text::new("hello")).into()
+    }
+
+    fn load(&self) -> Command<Message> {
+        Command::batch(vec![])
+    }
+}
+
+impl From<StakeholderState> for Box<dyn State> {
+    fn from(s: StakeholderState) -> Box<dyn State> {
+        Box::new(s)
+    }
+}
