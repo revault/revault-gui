c6ac0f0daceb6fb091fce8766eb2a6eb1c9f592a
edouard <edouard@revault.dev>
2021-01-14 13:50:15 +0100

    fix(ui): image include_bytes

--- src/ui/image.rs
+++ src/ui/image.rs
@@ -1,6 +1,6 @@
 use iced::widget::svg::{Handle, Svg};
 
-const LOGO: &'static [u8; 8052] = include_bytes!("../../static/images/revault-colored-logo.svg");
+const LOGO: &'static [u8] = include_bytes!("../../static/images/revault-colored-logo.svg");
 
 pub fn revault_colored_logo() -> Svg {
     let h = Handle::from_memory(LOGO.to_vec());
