815c7521b4fdbd2e986c5210719f51708b3b9d25
edouard <edouard@revault.dev>
2021-01-19 13:34:00 +0100

    ui: add and delete recipient

--- src/ui/view/manager.rs
+++ src/ui/view/manager.rs
@@ -1,6 +1,6 @@
 use iced::{
-    pick_list, scrollable, text_input, Column, Container, Element, HorizontalAlignment, Length,
-    Row, Scrollable, TextInput,
+    pick_list, scrollable, text_input, Checkbox, Column, Container, Element, HorizontalAlignment,
+    Length, Row, Scrollable, TextInput,
 };
 
 use crate::ui::{
@@ -8,7 +8,7 @@ use crate::ui::{
     component::{badge, button, card, navbar, separation, text, TransparentPickListStyle},
     error::Error,
     icon::{dot_icon, history_icon, home_icon, send_icon, settings_icon},
-    message::{ManagerSendOutputMessage, Menu, Message, Role},
+    message::{InputMessage, Menu, Message, RecipientMessage, Role},
     view::layout,
 };
 
@@ -251,12 +251,26 @@ impl ManagerSidebar {
 #[derive(Debug)]
 pub enum ManagerSendView {
     SelectOutputs(ManagerSelectOutputsView),
-    SelectInputs,
+    SelectInputs(ManagerSelectInputsView),
 }
 
 impl ManagerSendView {
     pub fn new() -> Self {
         Self::SelectOutputs(ManagerSelectOutputsView::new())
+    }
+
+    pub fn next(&self) -> ManagerSendView {
+        match self {
+            Self::SelectOutputs(_) => Self::SelectInputs(ManagerSelectInputsView::new()),
+            _ => Self::new(),
+        }
+    }
+
+    pub fn previous(&self) -> ManagerSendView {
+        match self {
+            Self::SelectInputs(_) => Self::SelectOutputs(ManagerSelectOutputsView::new()),
+            _ => Self::new(),
+        }
     }
 }
 
@@ -265,6 +279,7 @@ pub struct ManagerSelectOutputsView {
     scroll: scrollable::State,
     cancel_button: iced::button::State,
     next_button: iced::button::State,
+    new_output_button: iced::button::State,
 }
 
 impl ManagerSelectOutputsView {
@@ -273,18 +288,46 @@ impl ManagerSelectOutputsView {
             cancel_button: iced::button::State::new(),
             next_button: iced::button::State::new(),
             scroll: scrollable::State::new(),
+            new_output_button: iced::button::State::new(),
         }
     }
 
     pub fn view<'a>(
         &'a mut self,
         selected_outputs: Vec<Element<'a, Message>>,
+        valid: bool,
     ) -> Element<'a, Message> {
-        let mut col_outputs = Column::new();
-        for element in selected_outputs {
+        let mut col_outputs = Column::new()
+            .spacing(20)
+            .width(Length::Fill)
+            .align_items(iced::Align::Center);
+        for (i, element) in selected_outputs.into_iter().enumerate() {
+            if i > 0 {
+                col_outputs = col_outputs.push(separation().width(Length::Fill));
+            }
             col_outputs = col_outputs.push(element);
         }
-        let element: Element<_> = col_outputs.into();
+        let element: Element<_> = col_outputs.max_width(500).into();
+
+        let mut footer = Row::new().spacing(20).push(button::cancel(
+            &mut self.new_output_button,
+            Container::new(text::simple("Add recipient")).padding(10),
+            Message::AddRecipient,
+        ));
+
+        if valid {
+            footer = footer.push(Container::new(button::primary(
+                &mut self.next_button,
+                Container::new(text::simple("Continue")).padding(10),
+                Message::Next,
+            )));
+        } else {
+            footer = footer.push(Container::new(button::primary_disable(
+                &mut self.next_button,
+                Container::new(text::simple("Continue")).padding(10),
+                Message::None,
+            )));
+        }
         Container::new(
             Scrollable::new(&mut self.scroll).push(Container::new(
                 Column::new()
@@ -298,12 +341,18 @@ impl ManagerSelectOutputsView {
                             .width(Length::Shrink),
                         ),
                     )
-                    .push(element)
-                    .push(Container::new(button::primary(
-                        &mut self.next_button,
-                        Container::new(text::simple("Continue")),
-                        Message::Next,
-                    ))),
+                    .push(
+                        Container::new(element)
+                            .width(Length::Fill)
+                            .align_x(iced::Align::Center),
+                    )
+                    .push(
+                        Column::new()
+                            .push(footer)
+                            .width(Length::Fill)
+                            .align_items(iced::Align::Center),
+                    )
+                    .spacing(20),
             )),
         )
         .padding(20)
@@ -314,44 +363,176 @@ impl ManagerSelectOutputsView {
 }
 
 #[derive(Debug)]
-pub enum ManagerSendOutputView {
-    Display,
-    Edit {
-        address_input: text_input::State,
-        amount_input: text_input::State,
-    },
+pub struct ManagerSendOutputView {
+    address_input: text_input::State,
+    amount_input: text_input::State,
+    delete_button: iced::button::State,
 }
 
 impl ManagerSendOutputView {
-    pub fn new_edit() -> Self {
-        Self::Edit {
+    pub fn new() -> Self {
+        Self {
             address_input: text_input::State::focused(),
-            amount_input: text_input::State::focused(),
+            amount_input: text_input::State::new(),
+            delete_button: iced::button::State::new(),
         }
     }
-    pub fn view(&mut self, address: &str, amount: &u64) -> Element<ManagerSendOutputMessage> {
-        match self {
-            Self::Edit {
-                address_input,
-                amount_input,
-            } => {
-                let address = TextInput::new(
-                    address_input,
-                    "address",
-                    &address,
-                    ManagerSendOutputMessage::AddressEdited,
-                );
-                let amount = TextInput::new(
-                    amount_input,
-                    "amount",
-                    &format!("{}", amount),
-                    ManagerSendOutputMessage::AmountEdited,
-                );
-                Container::new(Column::new().push(address).push(amount))
-                    .width(Length::Units(150))
-                    .into()
+    pub fn view(
+        &mut self,
+        address: &str,
+        amount: &str,
+        warning_address: &bool,
+        warning_amount: &bool,
+    ) -> Element<RecipientMessage> {
+        let address = TextInput::new(
+            &mut self.address_input,
+            "Address",
+            &address,
+            RecipientMessage::AddressEdited,
+        )
+        .padding(10);
+        let mut col = Column::new()
+            .push(
+                Container::new(button::transparent(
+                    &mut self.delete_button,
+                    Container::new(text::simple("X Remove")).padding(10),
+                    RecipientMessage::Delete,
+                ))
+                .width(Length::Fill)
+                .align_x(iced::Align::End),
+            )
+            .push(text::bold("Enter address:"))
+            .push(address);
+
+        if *warning_address {
+            col = col.push(card::alert_warning(Container::new(text::simple(
+                "Please enter a valid bitcoin address",
+            ))))
+        }
+        col = col.push(text::bold("Enter amount:")).push(
+            TextInput::new(
+                &mut self.amount_input,
+                "0.0",
+                &format!("{}", amount),
+                RecipientMessage::AmountEdited,
+            )
+            .padding(10),
+        );
+
+        if *warning_amount {
+            col = col.push(card::alert_warning(Container::new(text::simple(
+                "Please enter a valid amount",
+            ))))
+        }
+        Container::new(col.spacing(10)).into()
+    }
+}
+
+#[derive(Debug)]
+pub struct ManagerSelectInputsView {
+    scroll: scrollable::State,
+    back_button: iced::button::State,
+    cancel_button: iced::button::State,
+    next_button: iced::button::State,
+    new_output_button: iced::button::State,
+}
+
+impl ManagerSelectInputsView {
+    pub fn new() -> Self {
+        ManagerSelectInputsView {
+            cancel_button: iced::button::State::new(),
+            back_button: iced::button::State::new(),
+            next_button: iced::button::State::new(),
+            scroll: scrollable::State::new(),
+            new_output_button: iced::button::State::new(),
+        }
+    }
+
+    pub fn view<'a>(
+        &'a mut self,
+        selected_inputs: Vec<Element<'a, Message>>,
+        valid: bool,
+    ) -> Element<'a, Message> {
+        let mut col_inputs = Column::new()
+            .spacing(20)
+            .width(Length::Fill)
+            .align_items(iced::Align::Center);
+        for (i, element) in selected_inputs.into_iter().enumerate() {
+            if i > 0 {
+                col_inputs = col_inputs.push(separation().width(Length::Fill));
             }
-            _ => Container::new(Column::new()).into(),
+            col_inputs = col_inputs.push(element);
         }
+        let element: Element<_> = col_inputs.max_width(500).into();
+
+        let mut footer = Column::new();
+        if valid {
+            footer = footer.push(Container::new(button::primary(
+                &mut self.next_button,
+                Container::new(text::simple("Continue")).padding(10),
+                Message::Next,
+            )));
+        } else {
+            footer = footer.push(Container::new(button::primary_disable(
+                &mut self.next_button,
+                Container::new(text::simple("Continue")).padding(10),
+                Message::None,
+            )));
+        }
+        Container::new(
+            Scrollable::new(&mut self.scroll).push(Container::new(
+                Column::new()
+                    .push(
+                        Row::new()
+                            .push(
+                                Column::new()
+                                    .push(button::transparent(
+                                        &mut self.back_button,
+                                        Container::new(text::simple("Go Back")).padding(10),
+                                        Message::Previous,
+                                    ))
+                                    .width(Length::Fill),
+                            )
+                            .push(
+                                Container::new(button::cancel(
+                                    &mut self.cancel_button,
+                                    Container::new(text::simple("X Close")).padding(10),
+                                    Message::Menu(Menu::Home),
+                                ))
+                                .width(Length::Shrink),
+                            ),
+                    )
+                    .push(
+                        Container::new(element)
+                            .width(Length::Fill)
+                            .align_x(iced::Align::Center),
+                    )
+                    .push(
+                        Column::new()
+                            .push(footer)
+                            .width(Length::Fill)
+                            .align_items(iced::Align::Center),
+                    )
+                    .spacing(20),
+            )),
+        )
+        .padding(20)
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .into()
     }
+}
+
+pub fn manager_send_input_view<'a>(
+    outpoint: &str,
+    amount: &u64,
+    selected: bool,
+) -> Element<'a, InputMessage> {
+    let checkbox =
+        Checkbox::new(selected, &format!("{}", outpoint), InputMessage::Selected).text_size(15);
+    let row = Row::new()
+        .push(checkbox)
+        .push(text::bold(&format!("{}", *amount as f64 / 100000000_f64)))
+        .spacing(20);
+    Container::new(row).width(Length::Fill).into()
 }
