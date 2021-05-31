82a7050cf9b63a23849e3a564df69ddf8fbd4000
edouard <m@edouard.paris>
2021-05-31 17:34:37 +0200

    test installer step: check descriptors

--- src/installer/step/mod.rs
+++ src/installer/step/mod.rs
@@ -377,3 +377,221 @@ impl From<Final> for Box<dyn Step> {
         Box::new(s)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{DefineCpfpDescriptor as DefineCpfpDescriptorStep, *};
+    use crate::installer::message::{DefineCpfpDescriptor, ParticipantXpub, *};
+    use crate::revaultd::config::Config;
+
+    const STAKEHOLDERS_XPUBS: [&str; 4] = [
+        "xpub6DEzq5DNPx2rPiZJ7wvFhxRKUKDoV1GwjFmFdaxFfbsw9HsHyxc9usoRUMxqJaMrwoXh4apahsGEnjAS4cVCBDgqsx5Groww22AdHbgxVDg", 
+        "xpub6F7Ltmsut73cbUNAzh44DkxncMeQfPtRzx7aoXjFbUdd7yofR2intU4b6QcsXot1jgmVjHB3iMybCLhtqvhAx3L4VPbGUz5fwuyNeTkypUP",
+        "xpub6CutNDrGhiD8GbjgKQWoTfzdRmoHJT8AcBxaV4NvWmo4dE5KKwpg2ukvgiCRwgZuJRXxKRsgRrrZiDZFJw1rLyAvY7X52WNEuaJXcVKLVFG", 
+        "xpub6EN35Df8V826n4HuW4QZEhFyyMq4jmou3AFnVqRpoFw8YS68ojkVNzVGWhnkCyGwZjVVUEoeBWhTfJ38C3Fvsc3ibvYFi5BvmQwAMZkqEqH"
+    ];
+
+    const MANAGERS_XPUBS: [&str; 2] = [
+        "xpub6CZFHPW1GiB8YgV7zGpeQDB6mMHZYPQyUaHrM1nMvKMgLxwok4xCtnzjuxQ3p1LHJUkz5i1Y7bRy5fmGrdg8UBVb39XdXNtWWd2wTsNd7T9",
+        "xpub6Doj75MBvKp7bgHxF1KeDGxm36rd4wonZWv8sfzTeNoNVX2QZaQdrEcs7NDXvs4Cbsy9TPMx5VDcMK6JjSKepBbYDPiJ9bLBR4bqfdHmxZx",
+    ];
+
+    const COSIGNERS_KEYS: [&str; 4] = [
+        "030f64b922aee2fd597f104bc6cb3b670f1ca2c6c49b1071a1a6c010575d94fe5a",
+        "02abe475b199ec3d62fa576faee16a334fdb86ffb26dce75becebaaedf328ac3fe",
+        "0314f3dc33595b0d016bb522f6fe3a67680723d842c1b9b8ae6b59fdd8ab5cccb4",
+        "025eba3305bd3c829e4e1551aac7358e4178832c739e4fc4729effe428de0398ab",
+    ];
+
+    fn load_stakeholders_xpubs(step: &mut dyn Step, xpubs: Vec<String>) {
+        let mut i = 0;
+        for xpub in xpubs {
+            step.update(Message::DefineStakeholderXpubs(
+                DefineStakeholderXpubs::AddXpub,
+            ));
+            step.update(Message::DefineStakeholderXpubs(
+                DefineStakeholderXpubs::StakeholderXpub(i, ParticipantXpub::XpubEdited(xpub)),
+            ));
+            i = i + 1;
+        }
+    }
+
+    fn load_managers_xpubs(step: &mut dyn Step, xpubs: Vec<String>) {
+        let mut i = 0;
+        for xpub in xpubs {
+            step.update(Message::DefineManagerXpubs(DefineManagerXpubs::AddXpub));
+            step.update(Message::DefineManagerXpubs(
+                DefineManagerXpubs::ManagerXpub(i, ParticipantXpub::XpubEdited(xpub)),
+            ));
+            i = i + 1;
+        }
+    }
+
+    fn load_cosigners_keys(step: &mut dyn Step, keys: Vec<String>) {
+        let mut i = 0;
+        for key in keys {
+            step.update(Message::DefineManagerXpubs(DefineManagerXpubs::AddCosigner));
+            step.update(Message::DefineManagerXpubs(
+                DefineManagerXpubs::CosignerKey(i, CosignerKey::KeyEdited(key)),
+            ));
+            i = i + 1;
+        }
+    }
+
+    #[test]
+    fn define_deposit_descriptor() {
+        let mut manager_step = manager::DefineStakeholderXpubs::new();
+        load_stakeholders_xpubs(
+            &mut manager_step,
+            vec![
+                STAKEHOLDERS_XPUBS[2].to_string(),
+                STAKEHOLDERS_XPUBS[1].to_string(),
+                STAKEHOLDERS_XPUBS[0].to_string(),
+                STAKEHOLDERS_XPUBS[3].to_string(),
+            ],
+        );
+
+        let mut manager_config = Config::new();
+        manager_step.edit_config(&mut manager_config);
+
+        let mut stakeholder_step = stakeholder::DefineStakeholderXpubs::new();
+        load_stakeholders_xpubs(
+            &mut stakeholder_step,
+            vec![
+                STAKEHOLDERS_XPUBS[3].to_string(),
+                STAKEHOLDERS_XPUBS[0].to_string(),
+                STAKEHOLDERS_XPUBS[1].to_string(),
+            ],
+        );
+        stakeholder_step.update(Message::DefineStakeholderXpubs(
+            DefineStakeholderXpubs::OurXpubEdited(STAKEHOLDERS_XPUBS[2].to_string()),
+        ));
+
+        let mut stakeholder_config = Config::new();
+        stakeholder_step.edit_config(&mut stakeholder_config);
+
+        assert_eq!(
+            manager_config.scripts_config.deposit_descriptor,
+            stakeholder_config.scripts_config.deposit_descriptor,
+        );
+    }
+
+    #[test]
+    fn define_unvault_descriptor() {
+        let mut manager_step = manager::DefineManagerXpubs::new();
+        manager_step.load_context(&Context {
+            number_cosigners: 4,
+            stakeholders_xpubs: vec![
+                STAKEHOLDERS_XPUBS[2].to_string(),
+                STAKEHOLDERS_XPUBS[1].to_string(),
+                STAKEHOLDERS_XPUBS[0].to_string(),
+                STAKEHOLDERS_XPUBS[3].to_string(),
+            ],
+        });
+
+        load_managers_xpubs(&mut manager_step, vec![MANAGERS_XPUBS[0].to_string()]);
+        load_cosigners_keys(
+            &mut manager_step,
+            vec![
+                COSIGNERS_KEYS[2].to_string(),
+                COSIGNERS_KEYS[1].to_string(),
+                COSIGNERS_KEYS[0].to_string(),
+                COSIGNERS_KEYS[3].to_string(),
+            ],
+        );
+
+        manager_step.update(Message::DefineManagerXpubs(
+            DefineManagerXpubs::OurXpubEdited(MANAGERS_XPUBS[1].to_string()),
+        ));
+
+        manager_step.update(Message::DefineManagerXpubs(
+            DefineManagerXpubs::ManagersTreshold(Action::Increment),
+        ));
+        manager_step.update(Message::DefineManagerXpubs(
+            DefineManagerXpubs::SpendingDelay(Action::Increment),
+        ));
+
+        let mut manager_config = Config::new();
+        manager_step.edit_config(&mut manager_config);
+
+        let mut stakeholder_step = stakeholder::DefineManagerXpubs::new();
+        stakeholder_step.load_context(&Context {
+            number_cosigners: 4,
+            stakeholders_xpubs: vec![
+                STAKEHOLDERS_XPUBS[3].to_string(),
+                STAKEHOLDERS_XPUBS[2].to_string(),
+                STAKEHOLDERS_XPUBS[0].to_string(),
+                STAKEHOLDERS_XPUBS[1].to_string(),
+            ],
+        });
+
+        load_managers_xpubs(
+            &mut stakeholder_step,
+            vec![MANAGERS_XPUBS[1].to_string(), MANAGERS_XPUBS[0].to_string()],
+        );
+        load_cosigners_keys(
+            &mut stakeholder_step,
+            vec![
+                COSIGNERS_KEYS[3].to_string(),
+                COSIGNERS_KEYS[2].to_string(),
+                COSIGNERS_KEYS[0].to_string(),
+                COSIGNERS_KEYS[1].to_string(),
+            ],
+        );
+        stakeholder_step.update(Message::DefineManagerXpubs(
+            DefineManagerXpubs::ManagersTreshold(Action::Increment),
+        ));
+        stakeholder_step.update(Message::DefineManagerXpubs(
+            DefineManagerXpubs::SpendingDelay(Action::Increment),
+        ));
+
+        let mut stakeholder_config = Config::new();
+        stakeholder_step.edit_config(&mut stakeholder_config);
+
+        assert_eq!(
+            manager_config.scripts_config.unvault_descriptor,
+            stakeholder_config.scripts_config.unvault_descriptor,
+        );
+    }
+
+    #[test]
+    fn define_cpfp_descriptor() {
+        let mut cpfp_1_step = DefineCpfpDescriptorStep::new();
+        cpfp_1_step.update(Message::DefineCpfpDescriptor(DefineCpfpDescriptor::AddXpub));
+        cpfp_1_step.update(Message::DefineCpfpDescriptor(
+            DefineCpfpDescriptor::ManagerXpub(
+                0,
+                ParticipantXpub::XpubEdited(MANAGERS_XPUBS[0].to_string()),
+            ),
+        ));
+        cpfp_1_step.update(Message::DefineCpfpDescriptor(DefineCpfpDescriptor::AddXpub));
+        cpfp_1_step.update(Message::DefineCpfpDescriptor(
+            DefineCpfpDescriptor::ManagerXpub(
+                1,
+                ParticipantXpub::XpubEdited(MANAGERS_XPUBS[1].to_string()),
+            ),
+        ));
+
+        let mut cpfp_1_config = Config::new();
+        cpfp_1_step.edit_config(&mut cpfp_1_config);
+
+        let mut cpfp_2_step = DefineCpfpDescriptorStep::new();
+        cpfp_2_step.update(Message::DefineCpfpDescriptor(DefineCpfpDescriptor::AddXpub));
+        cpfp_2_step.update(Message::DefineCpfpDescriptor(
+            DefineCpfpDescriptor::ManagerXpub(
+                0,
+                ParticipantXpub::XpubEdited(MANAGERS_XPUBS[1].to_string()),
+            ),
+        ));
+        cpfp_2_step.update(Message::DefineCpfpDescriptor(DefineCpfpDescriptor::AddXpub));
+        cpfp_2_step.update(Message::DefineCpfpDescriptor(
+            DefineCpfpDescriptor::ManagerXpub(
+                1,
+                ParticipantXpub::XpubEdited(MANAGERS_XPUBS[0].to_string()),
+            ),
+        ));
+
+        let mut cpfp_2_config = Config::new();
+        cpfp_2_step.edit_config(&mut cpfp_2_config);
+    }
+}
