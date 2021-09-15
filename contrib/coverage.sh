29642708db4d1aa4643d465e4e30cc6c05139ddc
Antoine Poinsot <darosior@protonmail.com>
2021-09-15 13:46:21 +0200

    contrib: add a script to measure test coverage
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

---
+++ contrib/coverage.sh
@@ -0,0 +1,17 @@
+set -ex
+
+rustup component add llvm-tools-preview
+if ! command -v grcov &>/dev/null; then
+    cargo +nightly install grcov
+fi
+
+cargo clean
+
+rm revault_gui_coverage_*.profraw
+LLVM_PROFILE_FILE="revault_gui_coverage_%m.profraw" RUSTFLAGS="-Zinstrument-coverage" RUSTDOCFLAGS="$RUSTFLAGS -Z unstable-options --persist-doctests target/debug/doctestbins" cargo +nightly build --all-features
+LLVM_PROFILE_FILE="revault_gui_coverage_%m.profraw" RUSTFLAGS="-Zinstrument-coverage" RUSTDOCFLAGS="$RUSTFLAGS -Z unstable-options --persist-doctests target/debug/doctestbins" cargo +nightly test --all-features
+
+grcov . --binary-path ./target/debug/ -t html --branch --ignore-not-existing --llvm -o ./target/grcov/
+firefox target/grcov/index.html
+
+set +ex
