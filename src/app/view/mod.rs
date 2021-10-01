78e8e0a4f3de39f06cc84b0b5350a055b6e8da08
edouard <m@edouard.paris>
2021-10-01 14:17:14 +0200

    Add LoadingModal view

--- src/app/view/mod.rs
+++ src/app/view/mod.rs
@@ -20,12 +20,12 @@ pub use stakeholder::{StakeholderCreateVaultsView, StakeholderDelegateFundsView}
 pub use vault::VaultView;
 pub use vaults::VaultsView;
 
-use iced::{scrollable, Column, Container, Element};
+use iced::{scrollable, Column, Container, Element, Length, Row};
 
 use crate::{
-    app::{context::Context, error::Error, message::Message},
+    app::{context::Context, error::Error, menu::Menu, message::Message},
     daemon::client::Client,
-    ui::component::{navbar, scroll},
+    ui::component::{button, card, navbar, scroll, text::Text, ContainerBackgroundStyle},
 };
 
 #[derive(Debug)]
@@ -56,5 +56,53 @@ impl LoadingDashboard {
             ))),
         )
         .into()
+    }
+}
+
+#[derive(Debug)]
+pub struct LoadingModal {
+    scroll: scrollable::State,
+    close_button: iced::button::State,
+}
+
+impl LoadingModal {
+    pub fn new() -> Self {
+        LoadingModal {
+            scroll: scrollable::State::new(),
+            close_button: iced::button::State::new(),
+        }
+    }
+
+    pub fn view<'a, C: Client>(
+        &'a mut self,
+        _ctx: &Context<C>,
+        warning: Option<&Error>,
+        close_redirect: Menu,
+    ) -> Element<'a, Message> {
+        let mut col = Column::new()
+            .push(
+                Row::new().push(Column::new().width(Length::Fill)).push(
+                    Container::new(
+                        button::close_button(&mut self.close_button)
+                            .on_press(Message::Menu(close_redirect)),
+                    )
+                    .width(Length::Shrink),
+                ),
+            )
+            .spacing(50);
+
+        if let Some(error) = warning {
+            col = col.push(card::alert_warning(Container::new(Text::new(&format!(
+                "{}",
+                error
+            )))))
+        }
+
+        Container::new(scroll(&mut self.scroll, Container::new(col)))
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .style(ContainerBackgroundStyle)
+            .padding(20)
+            .into()
     }
 }
